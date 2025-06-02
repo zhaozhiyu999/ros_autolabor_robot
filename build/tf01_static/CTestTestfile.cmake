12441382351e8fd46441ac30ed301dc6c6f6d32b
Zhiyu Zhao <zhiyu.zhao@tum.de>
2025-06-02 20:48:32 +0200

    test initial

---
+++ build/tf01_static/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/zzy/demo_04/src/tf01_static
+# Build directory: /home/zzy/demo_04/build/tf01_static
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ src/tf01_static/src/demo01_static_pub.cpp
@@ -0,0 +1,20 @@
+#include "ros/ros.h"
+
+/* 
+  静态坐标变换发布方:
+        发布关于 laser 坐标系的relative位置信息 
+
+    实现流程:
+        1.包含头文件
+        2.初始化 ROS 节点
+        3.创建静态坐标转换广播器
+        4.创建坐标系信息
+        5.广播器发布坐标系信息
+        6.spin()
+*/
+
+int main(int argc, char *argv[])
+{
+    /* code */
+    return 0;
+}
