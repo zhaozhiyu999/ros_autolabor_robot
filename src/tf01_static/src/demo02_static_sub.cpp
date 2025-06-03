7847e727802bde0b665345c1aef733329a8e2d1e
Zhiyu Zhao <zhiyu.zhao@tum.de>
2025-06-04 00:12:24 +0200

    static_transformation

---
+++ src/tf01_static/src/demo02_static_sub.cpp
@@ -0,0 +1,80 @@
+/*  
+    订阅坐标系信息，生成一个相对于 子级坐标系的坐标点数据，转换成父级坐标系中的坐标点
+
+    实现流程:
+        1.包含头文件
+        2.初始化 ROS 节点
+        3.创建 TF 订阅节点
+        4.生成一个坐标点(相对于子级坐标系)
+        5.转换坐标点(相对于父级坐标系)
+        6.spin()
+*/
+//1.包含头文件
+#include "ros/ros.h"
+#include "tf2_ros/transform_listener.h"
+#include "tf2_ros/buffer.h"
+#include "geometry_msgs/PointStamped.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h" //注意: 调用 transform 必须包含该头文件
+
+int main(int argc, char *argv[])
+{
+    setlocale(LC_ALL,"");
+    // 2.初始化 ROS 节点
+    ros::init(argc,argv,"tf_sub");
+    ros::NodeHandle nh;
+    // 3.创建 TF 订阅节点
+    tf2_ros::Buffer buffer;
+    tf2_ros::TransformListener listener(buffer);
+
+    ros::Rate r(1);
+    while (ros::ok())
+    {
+    // 4.生成一个坐标点(相对于子级坐标系)
+        geometry_msgs::PointStamped point_laser;
+        point_laser.header.frame_id = "laser";
+        point_laser.header.stamp = ros::Time::now();
+        point_laser.point.x = 1;
+        point_laser.point.y = 2;
+        point_laser.point.z = 7.3;
+    // 5.转换坐标点(相对于父级坐标系)
+        //新建一个坐标点，用于接收转换结果  
+        //--------------使用 try 语句或休眠，否则可能由于缓存接收延迟而导致坐标转换失败------------------------
+        try
+        {
+            geometry_msgs::PointStamped point_base;
+            point_base = buffer.transform(point_laser,"base_link");
+            ROS_INFO("转换后的数据:(%.2f,%.2f,%.2f),参考的坐标系是:",point_base.point.x,point_base.point.y,point_base.point.z,point_base.header.frame_id.c_str());
+
+        }
+        catch(const std::exception& e)
+        {
+            // std::cerr << e.what() << '\n';
+            ROS_INFO("程序异常.....");
+        }
+
+
+        r.sleep();  
+        ros::spinOnce();
+    }
+
+
+    return 0;
+}
+
+/* 
+补充1:
+当坐标系之间的相对位置固定时，那么所需参数也是固定的: 父系坐标名称、子级坐标系名称、x偏移量、y偏移量、z偏移量、x 翻滚角度、y俯仰角度、z偏航角度，实现逻辑相同，参数不同，那么 ROS 系统就已经封装好了专门的节点，使用方式如下:
+
+rosrun tf2_ros static_transform_publisher x偏移量 y偏移量 z偏移量 z偏航角度 y俯仰角度 x翻滚角度 父级坐标系 子级坐标系
+
+示例:rosrun tf2_ros static_transform_publisher 0.2 0 0.5 0 0 0 /baselink /laser
+
+也建议使用该种方式直接实现静态坐标系相对信息发布。
+
+补充2:
+可以借助于rviz显示坐标系关系，具体操作:
+
+新建窗口输入命令:rviz;
+在启动的 rviz 中设置Fixed Frame 为 base_link;
+点击左下的 add 按钮，在弹出的窗口中选择 TF 组件，即可显示坐标关系。
+*/
